49511d67956bb5858d647fd9198c18d7cb2018db
Ranjithv88 <ranjithkumar22445588@gmail.com>
2024-09-13 12:34:39 +0530

    last

--- src/main/java/com/App/Login/Controller/AuthenticationController.java
+++ src/main/java/com/App/Login/Controller/AuthenticationController.java
@@ -6,14 +6,12 @@ import com.App.Login.Service.AuthenticationService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/public")
+@CrossOrigin
 public class AuthenticationController {
 
     private final AuthenticationService service;
--- src/main/java/com/App/Login/Controller/TestingController.java
+++ src/main/java/com/App/Login/Controller/TestingController.java
@@ -1,11 +1,13 @@
 package com.App.Login.Controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/public")
+@RequestMapping("/user")
+@CrossOrigin("http://localhost:3000/home")
 public class TestingController {
 
         @GetMapping("/test")
